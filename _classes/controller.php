3ecebec32130852f84b4a7628a38bbff7017f9bb
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-05-02 18:14:12 +0000

    Views directory supports

--- _classes/controller.php
+++ _classes/controller.php
@@ -6,6 +6,7 @@ class controller{
     public function view($view,$data = array()){
         $request = $_GET;
         extract($data);
+        $view = str_replace('.','/',$view);
         return require "./views/".$view.".view.php";
     }
 }
